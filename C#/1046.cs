fdab62b81e96bb9f0edae150b67a2dc796e5bf16
Vinic-Web <souzavinifirmino@gmail.com>
2026-05-20 21:05:13 -0300

    adicionando 1046 c#

---
+++ C#/1046.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int[] valoresArray = ReadLine().Split().Select(int.Parse).ToArray();
+
+        int init = valoresArray[0];
+        int end = valoresArray[1];
+
+        if (init == end)
+        {
+            WriteLine("O JOGO DUROU 24 HORA(S)");
+        }
+        else if (init > end)
+        {
+            WriteLine($"O JOGO DUROU {Math.Abs((init - end) - 24)} HORA(S)");
+        }
+        else
+        {
+            WriteLine($"O JOGO DUROU {end-init} HORA(S)");
+        }
+    }
+}
